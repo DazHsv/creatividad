22714fcae10a657116627689f2494f2ca0886abf
Hugo Sanchez V <programacionhsv@gmail.com>
2016-04-26 17:57:50 -0500

    Arreglado problema de saludo y horas. Agregada fecha.

---
+++ clock/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+	var timer = document.getElementById('timer'),
+		title = document.querySelector('.title'),
+		meridian = document.getElementById('meridian'),
+		date = document.getElementById('date'),
+		container = document.getElementById('container'),
+		hcw = document.querySelector('.hours').style,
+		mcw = document.querySelector('.minutes').style,
+		scw = document.querySelector('.seconds').style;
+	var time, hours, minutes, seconds, message;
+
+	setInterval(function(){
+		container.classList.add('visible');
+	}, 1000);
+
+	setInterval(function(){
+
+		time = new Date();
+		hours = time.getHours();
+		minutes = time.getMinutes();
+		seconds = time.getSeconds();
+
+		message = ( (hours => 12) ? "Buenas tardes" : (hours => 19) ? "Buenas noches" : "Buenos días" ) + '!';
+
+		title.innerHTML = message;
+
+		date.innerHTML = time.getDate() + '/'
+						+ (time.getMonth > 10 ? '' : '0') + time.getMonth() + '/'
+						+ time.getFullYear();
+
+		timer.innerHTML = ((hours < 10 || hours > 13 || hours < 22) ? '0' : '')
+						+ ( hours < 13 ? hours : hours - 12 )  + ':'
+						+ ( minutes < 10 ? '0' : '') + minutes + ':'
+						+ ( seconds < 10 ? '0' : '') + seconds;
+
+		meridian.innerHTML = (hours < 12 ? 'a.m.' : 'p.m.');
+
+		hcw.boxShadow = ( (240/24)*hours )   + "px 0 0 #fff inset";
+		mcw.boxShadow = ( (240/60)*minutes ) + "px 0 0 #FF5722 inset";
+		scw.boxShadow = ( (240/60)*seconds ) + "px 0 0 #FFEB3B inset";
+
+	}, 1000);
+
+});
